c44414f27c53e31f495ecfa46580183a0c273361
mingsnx <mingsn456@gmail.com>
2018-05-20 17:57:43 +0800

    MK-MoonlightGoddess

---
+++ MK.Project/MK.MoonlightGoddess.Web/Controllers/VerifyLoginController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MK.MoonlightGoddess.Web.Controllers
+{
+    public class VerifyLoginController : Controller
+    {
+        // GET: VerifyLogin
+        
+        /// <summary>
+        /// 重写控制器访问之前的处理
+        /// 暂留，后面再写
+        /// 2018.5.20
+        /// </summary>
+        /// <param name="filterContext"></param>
+        protected override void OnActionExecuting(ActionExecutingContext filterContext)
+        {
+            base.OnActionExecuting(filterContext);
+        }
+    }
+}
---
+++ MK.Project/MK.MoonlightGoddess.Web/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="MK.MoonlightGoddess.Web.MvcApplication" Language="C#" %>
