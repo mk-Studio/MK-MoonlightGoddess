23be1c6d015775e24a0ff24d64e81657ec9d2b51
mingsn <mingsn456@gmail.com>
2018-05-21 14:27:21 +0800

    form

--- MK.Project/MK.MoonlightGoddess.Web/App_Start/RouteConfig.cs
+++ MK.Project/MK.MoonlightGoddess.Web/App_Start/RouteConfig.cs
@@ -16,7 +16,7 @@ namespace MK.MoonlightGoddess.Web
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
-                defaults: new { controller = "Index", action = "Index", id = UrlParameter.Optional }
+                defaults: new { controller = "Login", action = "Index", id = UrlParameter.Optional }
             );
         }
     }
