39c9abe99b3040421c1ac035e106838c5d897f4f
KevinTing7 <229412150@qq.com>
2018-05-24 13:52:32 +0800

    jm

--- MK.Project/MK.MoonlightGoddess.Web/Controllers/LoginController.cs
+++ MK.Project/MK.MoonlightGoddess.Web/Controllers/LoginController.cs
@@ -1,7 +1,9 @@
-﻿using MK.MoonlightGoddess.Models.EntityModels;
+﻿using MK.MoonlightGoddess.Core;
+using MK.MoonlightGoddess.Models.EntityModels;
 using MK.MoonlightGoddess.Service;
 using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
@@ -22,6 +24,14 @@ namespace MK.MoonlightGoddess.Web.Controllers
             var jsonResult = ServiceContent<MK_Info_User>.SelectSingle(model, "MK_Info_User", "ValidateLogon");
             if (!string.IsNullOrEmpty(jsonResult))
             {
+                DataTable Userdt = new DataTable();
+                Userdt = (DataTable)ServiceContent<MK_Info_User>.SelectSIDU(model, "MK_Info_User", "ValidateLogon");
+                foreach (DataColumn dr in Userdt.Columns)
+                {
+                    string ColumnsTitle = dr.ToString();
+                    string strValue = EncryptHelper.GetMD5_32(Userdt.Rows[0][ColumnsTitle].ToString());
+                    SessionHelper.SetSession(ColumnsTitle, strValue);
+                }
                 return Json(new { result = "Y" });
             }
             else
