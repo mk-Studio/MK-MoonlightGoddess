c11e063beb0c37e024ea946fc0d459832f5b72e6
mingsnx <mingsn456@gmail.com>
2018-05-25 14:53:54 +0800

    更新MKCom.js的下拉框数据绑定调用

--- MK.Project/MK.MoonlightGoddess.Web/Js/MKCom.js
+++ MK.Project/MK.MoonlightGoddess.Web/Js/MKCom.js
@@ -1,5 +1,8 @@
 ﻿
-function InitCombobox(name)
+//id：指定下拉框的id属性，也可以为class
+//name：sqlname名称
+//调用示列：InitCombobox("#id","GetUserType")
+function InitCombobox(id,name)
 {
     $.ajax({
         url: "/api/SeletcOptions/GetSelectOptions?name=" + name,
@@ -10,9 +13,9 @@ function InitCombobox(name)
         success: function (r) {
             for (var i = 0; i < r.length; i++) {
                 if (i === 0) {
-                    jQuery("<option value='All'>全部</option>").appendTo("#wuliaoType");
+                    jQuery("<option value='All'>全部</option>").appendTo(id);
                 }
-                jQuery("<option value='" + r[i].Value + "'>" + r[i].Text + "</option>").appendTo("#wuliaoType");
+                jQuery("<option value='" + r[i].Value + "'>" + r[i].Text + "</option>").appendTo(id);
             }
             form.render('select');
         }
