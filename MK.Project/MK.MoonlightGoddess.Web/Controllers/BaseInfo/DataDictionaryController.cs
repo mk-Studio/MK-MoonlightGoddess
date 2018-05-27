b193b3e4787d8d4a264bd7602f4f9ed822f63565
mingsn <mingsn456@gmail.com>
2018-05-27 13:49:46 +0800

    添加基础数据菜单（+物料信息模块）

---
+++ MK.Project/MK.MoonlightGoddess.Web/Controllers/BaseInfo/DataDictionaryController.cs
@@ -0,0 +1,31 @@
+﻿using MK.MoonlightGoddess.Models.EntityModels;
+using MK.MoonlightGoddess.Service;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MK.MoonlightGoddess.Web.Controllers.BaseInfo
+{
+    /// <summary>
+    /// 数据字典
+    /// </summary>
+    public class DataDictionaryController : Controller
+    {
+        // GET: DataDictionary
+        public ActionResult Index()
+        {
+            return View("../BaseInfoViews/DataDictionary");
+        }
+
+        [HttpPost]
+        public JsonResult GetQueryInfoWuLiao(MK_Info_WuLiao model)
+        {
+            var jsonResult = ServiceContent<MK_Info_WuLiao>.Select(model, "MK_Info_WuLiao", "QueryToInfoWuLiao");
+            return Json(jsonResult);
+        }
+
+
+    }
+}
