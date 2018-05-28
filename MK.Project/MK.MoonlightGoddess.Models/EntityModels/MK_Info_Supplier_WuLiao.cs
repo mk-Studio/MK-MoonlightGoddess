3dacad175860772461ce6f64c06d0f41c3eee89d
mingsn <mingsn456@gmail.com>
2018-05-29 00:21:46 +0800

    添加layui3.x版本、多选下拉框插件formSelects-v3.js，基础数据+30%完成度

---
+++ MK.Project/MK.MoonlightGoddess.Models/EntityModels/MK_Info_Supplier_WuLiao.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MK.MoonlightGoddess.Models.EntityModels
+{
+    public class MK_Info_Supplier_WuLiao:BaseModel
+    {
+        public string SupplierID { get; set; }
+        public string ShangPinID { get; set; }
+    }
+}
