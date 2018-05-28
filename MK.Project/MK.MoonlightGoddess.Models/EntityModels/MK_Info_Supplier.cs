633407fdf9b79e3cb4e7d9de260c8f729a7f96f3
mingsn <mingsn456@gmail.com>
2018-05-29 00:21:46 +0800

    添加layui3.x版本、多选下拉框插件formSelects-v3.js，基础数据+30%完成度

---
+++ MK.Project/MK.MoonlightGoddess.Models/EntityModels/MK_Info_Supplier.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MK.MoonlightGoddess.Models.EntityModels
+{
+    public class MK_Info_Supplier:BaseModel
+    {
+        public string SupplierName { get; set; }
+        public string LianXiRen { get; set; }
+        public string LianXiFangShi { get; set; }
+        public string Address { get; set; }
+    }
+}
--- MK.Project/MK.MoonlightGoddess.Web/Controllers/BaseInfo/Form/FormDataDictionaryController.cs
+++ MK.Project/MK.MoonlightGoddess.Web/Controllers/BaseInfo/Form/FormDataDictionaryController.cs
@@ -6,12 +6,22 @@ using System.Web.Mvc;
 
 namespace MK.MoonlightGoddess.Web.Controllers.BaseInfo.Form
 {
-    public class FormDataDictionaryController : Controller
+    public class FormDataDictionaryController : VerifyLoginController
     {
         // GET: FormDataDictionary
         public ActionResult FormInfoWuLiao()
         {
             return View("../BaseInfoViews/Form/FormInfoWuLiao");
+        }
+
+        public ActionResult FormTypeWuLiao()
+        {
+            return View("../BaseInfoViews/Form/FormTypeWuLiao");
+        }
+
+        public ActionResult FormInfoSupplier()
+        {
+            return View("../BaseInfoViews/Form/FormInfoSupplier");
         }
     }
 }
