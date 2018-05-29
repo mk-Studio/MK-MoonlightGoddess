fd28a144a570f81eb00186e51e27c1dd73582761
KevinTing7 <229412150@qq.com>
2018-05-29 14:55:41 +0800

    分组信息

--- MK.Project/MK.MoonlightGoddess.Models/EntityModels/MK_Info_PowerGroup.cs
+++ MK.Project/MK.MoonlightGoddess.Models/EntityModels/MK_Info_PowerGroup.cs
@@ -9,6 +9,11 @@ namespace MK.MoonlightGoddess.Models.EntityModels
     public class MK_Info_PowerGroup:BaseModel
     {
         /// <summary>
+        /// 上级节点
+        /// </summary>
+        public string OnID { get; set; }
+
+        /// <summary>
         /// 权限组名
         /// </summary>
         public string PowerGroupName { get; set; }
