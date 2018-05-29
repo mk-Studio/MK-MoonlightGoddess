8d7133d2f516261e4c69855c519acf7a7e67e57b
KevinTing7 <229412150@qq.com>
2018-05-29 14:55:20 +0800

    权限分配

---
+++ MK.Project/MK.MoonlightGoddess.Models/EntityModels/MK_Info_PowerAllot.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MK.MoonlightGoddess.Models.EntityModels
+{
+    public class MK_Info_PowerAllot:BaseModel
+    {
+        /// <summary>
+        /// 权限组ID
+        /// </summary>
+        public string PowerGroupID { get; set; }
+
+        /// <summary>
+        /// 权限ID（菜单ID）
+        /// </summary>
+        public string PowerID { get; set; }
+
+        /// <summary>
+        /// 权限标识
+        /// </summary>
+        public string Status { get; set; }
+
+        /// <summary>
+        /// 菜单名称
+        /// </summary>
+        public string PowerName { get; set; }
+
+        /// <summary>
+        /// 菜单备注
+        /// </summary>
+        public string Notes { get; set; }
+
+        /// <summary>
+        /// 菜单地址
+        /// </summary>
+        public string MenuAddress { get; set; }
+    }
+}
