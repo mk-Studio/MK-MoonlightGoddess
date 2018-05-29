5b474d798857db5ec9164a332630b89cf1d567e1
KevinTing7 <229412150@qq.com>
2018-05-29 14:56:07 +0800

    分组树类方法

---
+++ MK.Project/MK.MoonlightGoddess.Models/ResultModels/LayuiTreeResultModel.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MK.MoonlightGoddess.Models.ResultModels
+{
+    public class LayuiTreeResultModel
+    {
+        public string id { get; set; }
+        public string name { get; set; }
+        public List<LayuiTreeResultModel> children { get; set; }
+    }
+    public class TextTree
+    {
+        public List<LayuiTreeResultModel> result { get; set; }
+        public List<LayuiTreeResultModel> PowerGroup(System.Data.DataTable data)
+        {
+            List<LayuiTreeResultModel> result = new List<LayuiTreeResultModel>();
+            LayuiTreeResultModel nodes = new LayuiTreeResultModel();
+            DataRow root = data.Select("id='00000000-0000-0000-0000-000000000000'")[0];
+            nodes.id = root["id"].ToString();
+            nodes.name = root["name"].ToString();
+            List<LayuiTreeResultModel> outList = null;
+            AddChildren(data, root["id"].ToString(), out outList);
+            nodes.children = outList;
+            result.Add(nodes);
+            return result;
+        }
+        public void AddChildren(DataTable data,string pId,out List<LayuiTreeResultModel> outList)
+        {
+            List<LayuiTreeResultModel> _root = new List<LayuiTreeResultModel>();
+            System.Data.DataRow[] nodes = data.Select("onid='" + pId + "'");
+            foreach (System.Data.DataRow row in nodes)
+            {
+                LayuiTreeResultModel node = new LayuiTreeResultModel();
+                node.id = row["id"].ToString();
+                node.name = row["name"].ToString();
+                List<LayuiTreeResultModel> _outList = null;
+                AddChildren(data, row["id"].ToString(), out _outList);
+                node.children = _outList;
+                _root.Add(node);
+            }
+            outList = _root;
+        }
+    }
+}
