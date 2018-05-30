b76172de36f958ae6d82e7777502bb7153ad35b9
mingsn <mingsn456@gmail.com>
2018-05-30 20:52:02 +0800

    更新api上传、基础数据所有显示标记的状态更新

---
+++ MK.Project/MK.MoonlightGoddess.Models/ResultModels/LayuiUploadResultModel.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MK.MoonlightGoddess.Models.ResultModels
+{
+    public class LayuiUploadResultModel
+    {
+        public int code { get; set; }
+        public string msg { get; set; }
+        public object data { get; set; }
+
+        private LayuiUploadResultModel(int code,string msg,object data)
+        {
+            this.code = code;
+            this.msg = msg;
+            this.data = data;
+        }
+
+        public static Object CreateResult(int code, string msg, object data)
+        {
+            return new LayuiUploadResultModel(code, msg, data);
+        }
+    }
+}
