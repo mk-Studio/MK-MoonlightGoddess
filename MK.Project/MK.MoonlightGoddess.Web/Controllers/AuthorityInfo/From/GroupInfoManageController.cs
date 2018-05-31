584085bfbf2dec016c20a9ba7ccb34622aabe331
KevinTing7 <229412150@qq.com>
2018-06-01 02:49:30 +0800

    分组管理

---
+++ MK.Project/MK.MoonlightGoddess.Web/Controllers/AuthorityInfo/From/GroupInfoManageController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MK.MoonlightGoddess.Web.Controllers.AuthorityInfo.From
+{
+    public class GroupInfoManageController : Controller
+    {
+        // GET: GroupInfoManage
+        public ActionResult AddGroupInfo()
+        {
+            return View("../Authority/Form/FromAddGroupInfo.cshtml");
+        }
+        public ActionResult EditGroupInfo()
+        {
+            return View("../Authority/Form/FromEditGroupInfo.cshtml");
+        }
+    }
+}
