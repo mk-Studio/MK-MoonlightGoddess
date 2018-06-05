0af4f5686a372a66b332f275d7a2a19ef7571fe1
mingsn <mingsn456@gmail.com>
2018-06-05 23:45:42 +0800

    添加验证api=>VerifyDbData、验证数据库信息是否存在、添加MKCom.js=>ajaxVerify函数

---
+++ MK.Project/MK.MoonlightGoddess.Web/Controllers/VerifyDbDataController.cs
@@ -0,0 +1,87 @@
+﻿using MK.MoonlightGoddess.Models;
+using MK.MoonlightGoddess.Service;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web;
+using System.Web.Http;
+
+namespace MK.MoonlightGoddess.Web.Controllers
+{
+    public class VerifyDbDataController : ApiController
+    {
+        public class VerifyRequestParams
+        {
+            /// <summary>
+            /// 校准ID
+            /// </summary>
+            public string ID { get; set; }
+
+            /// <summary>
+            /// find sqlcmd by sql name 
+            /// </summary>
+            public string SqlName { get; set; }
+
+            /// <summary>
+            /// 物料类型名称
+            /// </summary>
+            public string WuLiaoTypeName { get; set; }
+
+            /// <summary>
+            /// 物料商品名称
+            /// </summary>
+            public string ShangPinName { get; set; }
+
+            /// <summary>
+            /// 物料商品规格
+            /// </summary>
+            public string GuiGe { get; set; }
+
+            /// <summary>
+            /// 业务类型名称
+            /// </summary>
+            public string YeWuType { get; set; }
+
+            /// <summary>
+            /// 物料公司名称
+            /// </summary>
+            public string Name { get; set; }
+
+            /// <summary>
+            /// 用户名称
+            /// </summary>
+            public string UserName { get; set; }
+
+            /// <summary>
+            /// 供应商名称
+            /// </summary>
+            public string SupplierName { get; set; }
+
+            /// <summary>
+            /// 币别名称
+            /// </summary>
+            public string CurrencyName { get; set; }
+
+            /// <summary>
+            /// 币别代码
+            /// </summary>
+            public string CurrencyCode { get; set; }
+
+            
+        }
+        //Verify
+        [HttpPost]
+        public IHttpActionResult VerifyExists(VerifyRequestParams model)
+        {
+            HttpContext context = HttpContext.Current;
+            context.Response.AddHeader("Access-Control-Allow-Origin", "*");
+            var _data = ServiceContent<dynamic>.SelectSingle(model, "VerifyDbData", model.SqlName);
+            var jsonResult = _data == "OK"
+                ? AjaxResultModel.CreateMessage(false, "NOT EXISTS", 0, _data)
+                : AjaxResultModel.CreateMessage(false, "EXISTS", 1, _data);
+            return Json(jsonResult);
+        }
+    }
+}
--- MK.Project/MK.MoonlightGoddess.Web/Js/MKCom.js
+++ MK.Project/MK.MoonlightGoddess.Web/Js/MKCom.js
@@ -2,8 +2,7 @@
 //id：指定下拉框的id属性，也可以为class
 //name：sqlname名称
 //调用示列：InitCombobox("#id","GetUserType")
-function InitCombobox(id,name,form)
-{
+function InitCombobox(id, name, form) {
     $.ajax({
         url: "/api/SeletcOptions/GetSelectOptions?name=" + name,
         type: "get",
@@ -22,7 +21,7 @@ function InitCombobox(id,name,form)
     })
 }
 
-function InitNewCombobox(id, name, form,hasAll) {
+function InitNewCombobox(id, name, form, hasAll) {
     $.ajax({
         url: "/api/SeletcOptions/GetSelectOptions?name=" + name,
         type: "get",
@@ -37,6 +36,23 @@ function InitNewCombobox(id, name, form,hasAll) {
                 jQuery("<option value='" + r[i].Value + "'>" + r[i].Text + "</option>").appendTo(id);
             }
             form.render('select');
+        }
+    })
+}
+
+//验证是否存在，存在返回true，不存在返回false
+var ajaxVerify = (data, notexistsCallback, callback) => {
+    $.ajax({
+        type: "post",
+        url: "/api/VerifyDbData/VerifyExists",
+        contentType: 'application/json',
+        data: JSON.stringify(data),
+        success: function (r) {
+            if (r.Msg === "EXISTS") {
+                notexistsCallback(r);
+            } else {
+                callback(r);
+            }
         }
     })
 }
