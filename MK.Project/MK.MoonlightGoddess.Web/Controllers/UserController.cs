6012192c4d3f63c619a64ddd309d9d3683da7819
mingsn <mingsn456@gmail.com>
2018-06-06 20:06:45 +0800

    添加账号设置页面

--- MK.Project/MK.MoonlightGoddess.Web/Controllers/UserController.cs
+++ MK.Project/MK.MoonlightGoddess.Web/Controllers/UserController.cs
@@ -6,10 +6,15 @@ using System.Web.Mvc;
 
 namespace MK.MoonlightGoddess.Web.Controllers
 {
-    public class UserController : Controller
+    public class UserController : VerifyLoginController
     {
         // GET: User
         public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult SecuritySettings()
         {
             return View();
         }
