dbf4fb097420c148248ed2a982de6da1895d6f29
mingsn <mingsn456@gmail.com>
2018-06-18 15:15:30 +0800

    添加审批模块

---
+++ MK.Project/MK.MoonlightGoddess.Web/Controllers/Approved/ApprovalsController.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MK.MoonlightGoddess.Web.Controllers.Approved
+{
+    public class ApprovalsController : Controller
+    {
+        // GET: Approvals
+        
+        // 发起审批
+        public ActionResult InitiateApprovals()
+        {
+            return View("../Approvals/InitiateApprovals");
+        }
+
+        // 待我审批的
+        public ActionResult WaitingMyApprovals()
+        {
+            return View("../Approvals/WaitingMyApprovals");
+        }
+
+        // 我已审批的
+        public ActionResult IHaveApprovals()
+        {
+            return View("../Approvals/IHaveApprovals");
+        }
+
+        // 我发起的
+        public ActionResult MyInitiateApprovals()
+        {
+            return View("../Approvals/MyInitiateApprovals");
+        }
+
+        // 抄送我的
+        public ActionResult CCMyApprovals()
+        {
+            return View("../Approvals/CCMyApprovals");
+        }
+    }
+}
